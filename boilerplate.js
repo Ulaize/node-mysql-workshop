88e0541357bcd7841af02ec36719b3b3d6774508
Ulaize <ulaize@creative-tweed.com>
2015-09-30 18:57:36 +0000

    Exercise 1

--- boilerplate.js
+++ boilerplate.js
@@ -1,23 +1,37 @@
 var mysql = require('mysql');
 var Promise = require('bluebird');
+var Table = require('cli-table');
+var colors = require('colors');
 Promise.promisifyAll(mysql);
 Promise.promisifyAll(require("mysql/lib/Connection").prototype);
 Promise.promisifyAll(require("mysql/lib/Pool").prototype);
+
 
 var connection = mysql.createConnection({
   host     : process.env.IP,
   user     : process.env.C9_USER,
   password : '',
-  database : 'addressbook'
+  database : 'mysql'
 });
 
-connection.queryAsync("SELECT * FROM Account").then(
+
+connection.queryAsync("SHOW DATABASES").then(
     function(results) {
-    	var rows = results[0];
-    	// do something with rows...
-    }
-).finally(
+        var rows = results[0];
+        
+        var table = new Table({
+            head: ['Database']
+        });
+        
+        for (var i = 0; i < rows.length ; i++){
+            table.push([rows[i].Database]);
+        }
+
+        console.log(table.toString());
+        
+}).finally(
     function() {
         connection.end();
     }
 );
+
